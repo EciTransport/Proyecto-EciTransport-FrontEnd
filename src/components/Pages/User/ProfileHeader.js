09eb3a008379ced63fa8aeb50dd402a502c2d46a
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-10-25 23:29:27 -0500

    cambios azure

--- src/components/Pages/User/ProfileHeader.js
+++ src/components/Pages/User/ProfileHeader.js
@@ -4,6 +4,24 @@ import './profile.css'
 import {Container, ContainerReport} from './styles';
 import {useState, useEffect} from 'react';
 import { usuario } from '../../Utils/user'
+import { callMsGraph } from "../../../graph";
+import { loginRequest } from "../../../authConfig";
+import { useMsal } from "@azure/msal-react";
+
+var userId = '';
+
+const ProfileData = (props) => {
+    userId = props.graphData.userPrincipalName;
+    return (
+        <div className="profileInfo">
+            <h4 className='profileInfoName'>{props.graphData.givenName}
+            {props.graphData.surname}
+            </h4>
+            <span id="userId" className="profileInfoDesc">{props.graphData.userPrincipalName}</span>
+        </div>
+    );
+};
+
 
 const HeaderProfile = () => {
 
@@ -13,12 +31,12 @@ const HeaderProfile = () => {
   const [user, setUser] = useState([]);
 
   useEffect( () => {
-    fetch('http://localhost:8080/v1/user/id/' + usuario)
+    fetch('http://localhost:8080/v1/user/email/' + userId)
     .then(response => response.json())
     .then((data) => setUser(data.value)) } , [] );
 
   useEffect( () => {
-    fetch('http://localhost:8080/v1/reports/reportsUser/' + usuario)
+    fetch('http://localhost:8080/v1/reports/reportsUser/' + user.id)
     .then(response => response.json())
     .then(data => setReports(data))} , [] );
 
@@ -44,9 +62,11 @@ const HeaderProfile = () => {
                     />
                     </div>
                     <div className="profileInfo">
-                        <h4 className="profileInfoName">{user.nombre}</h4>
+                        {/* <h4 className="profileInfoName">{user.nombre}</h4>
                         <span className="profileInfoDesc">{user.id}</span>
-                        <span className="profileInfoDesc">{user.email}</span>
+                        <span className="profileInfoDesc">{user.email}</span> */}
+                        <ProfileContent />
+
                     </div>
                     <ContainerReport>
                         <h2 className="report">Report History</h2>
@@ -59,7 +79,46 @@ const HeaderProfile = () => {
         </div>
 
     </Container>
+
   )
 }
 
 export {HeaderProfile}
+
+
+
+
+
+function ProfileContent() {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    const name = accounts[0] && accounts[0].name;
+
+    function RequestProfileData() {
+        const request = {
+            ...loginRequest,
+            account: accounts[0]
+        };
+
+        // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+        instance.acquireTokenSilent(request).then((response) => {
+            callMsGraph(response.accessToken).then(response => setGraphData(response));
+        }).catch((e) => {
+            instance.acquireTokenPopup(request).then((response) => {
+                callMsGraph(response.accessToken).then(response => setGraphData(response));
+            });
+        });
+    }
+
+    return (
+        <>
+            <h5 className="card-title">Welcome {name}</h5>
+            {graphData ? 
+                <ProfileData graphData={graphData} />
+                :
+                RequestProfileData()
+            }
+        </>
+    );
+};
