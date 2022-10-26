abf888da1d433e3f887974a6eec5b435d0e528b0
Yesid Mora <yesid.mora@mail.escuelaing.edu.co>
2022-10-26 01:01:18 -0500

    Cambios css

--- src/components/Generals/Login/index.js
+++ src/components/Generals/Login/index.js
@@ -3,26 +3,43 @@ import { useState } from 'react';
 import { routes } from '../../Utils/routes';
 import { usuario, setUsuarios } from '../../Utils/user';
 import { useNavigate } from "react-router-dom";
-
+import CarCrashIcon from '@mui/icons-material/CarCrash';
+import { Label } from '@mui/icons-material';
+import { Button, Input } from '@mui/material';
+import './style.css';
 const Login = () => {
 
-  const [title, setTitle] = useState('')
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
 
   const navigate = useNavigate();
 
   function redirect() {
-    setUsuarios(title)
     navigate(routes.home.path)
   }
 
   return (
-    <div>
-      <label>Digite el codigo de Usuario: </label>
+    <div className="login">
 
-      <input onChange={event => setTitle(event.target.value)} />
+      <header className="header">
+          <CarCrashIcon className="icon"/>
+          <h1 className="title">EciTransport</h1>
+      </header>
 
-      <button onClick={() => redirect()}>Entra</button>
-
+      <div className="label">
+        <div className='comp'>
+          <Label className="labels" >User</Label>
+          <Input className="input" type="text" placeholder='user' onChange={event => setUser(event.target.value)} />
+        </div>
+        
+        <div className='comp'>
+          <Label className="labels">Password</Label>
+          <Input placeholder='Password' type="text" onChange={event => setPassword(event.target.value)}/>
+        </div>
+      
+        <Button onClick={() => redirect()}>Entra</Button>
+      
+      </div>
     </div>
   )
 }
